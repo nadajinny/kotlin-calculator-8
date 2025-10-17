997e0fc9218d1609758aabf8ef8b6703b4e05414
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-10-17 18:33:45 +0900

    feat(isEmptyInput): 사용자 입력 값이 빈 문자열이거나 비어있을 경우, 0을 반환 기능 구현

---
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -0,0 +1,12 @@
+package calculator
+
+class StringCalculator {
+    fun run(input: String): Int {
+        //빈 문자열 처리
+        if(input.isEmpty()) return 0
+        if(input.isBlank()) return 0
+
+        val sum = 1
+        return sum
+    }
+}
