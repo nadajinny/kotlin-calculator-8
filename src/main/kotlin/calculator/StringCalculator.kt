4cc6099efda70c6b1f68e51460d5cfcae8588df4
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-10-19 15:25:34 +0900

    feat(add): 숫자 리스트를 전달할 경우, 해당 리스트 안에 있는 모든 수 합산

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,11 +2,15 @@ package calculator
 
 class StringCalculator {
     fun run(input: String): Int {
-        //빈 문자열 처리
         if(input.isEmpty()) return 0
         if(input.isBlank()) return 0
 
-        val sum = 1
-        return sum
+        val spliter = mutableListOf(",", ":")
+        val numberlist = mutableListOf<Int>()
+        var current = StringBuilder()
+
+        fun add(list: List<Int>): Int {
+            return list.sum()
+        }
     }
 }
